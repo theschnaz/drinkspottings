26947d8cc47e360b75cdbc264b5a9af7e632c6d2
Gregory Schnese <greg@coovents.com>
2012-05-21 23:06:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drinkspottings::Application.config.secret_token = '33e9843f3f542fcb362836f751c2ce6589edd4b83764a85e08219dab64d45332a370875c2f20b4c60a5abe6fa47966a16b4167abca933b7404d89b27deadf547'
