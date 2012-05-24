d67b9e086f07707c753cb15497a7a41e1f1267b0
Gregory Schnese <greg@coovents.com>
2012-05-23 22:48:30 -0400

    trying to save the image to s3 and the model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,7 @@ class PostsController < ApplicationController
 
   def post_email
   
-    @post = Post.new
-    @post.photo = params[:attachment]
+    @post = Post.new(:photo => params[:attachment])
     @post.save
     
     render :text => "email sent", :status => 200
