9ebc05778badff1f0b3b909d5f9cfad97777d5fe
Gregory Schnese <greg@coovents.com>
2012-05-23 22:37:56 -0400

    trying to attach image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,9 @@ class PostsController < ApplicationController
 
   def post_email
   
-    #@post = Post.new
-    #@post.save
+    @post = Post.new
+    @post.photo = params[:attachment]
+    @post.save
     
     render :text => "email sent", :status => 200
   
