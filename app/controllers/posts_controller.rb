3dfcc3b9425ae180e4fd371aee3c1eb0199b4208
Gregory Schnese <greg@coovents.com>
2012-05-30 22:44:40 -0400

    grr

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def post_email
   
-    @post = Post.new(:photo => params[:attachment])
+    @post = Post.new
     @post.save
     
     render :text => "email sent", :status => 200
