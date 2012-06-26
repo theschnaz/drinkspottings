e428074a25f937bdba1c30cbbd983b45c1f679f4
Gregory Schnese <greg@coovents.com>
2012-06-25 22:30:44 -0400

    fixing more bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.name = auth["info"]["name"]
-      user.user_name = auth["info"]["nickname"]
+      user.username = auth["info"]["nickname"]
       user.email = auth["info"]["email"]
       user.facebook_key = auth["credentials"]["token"]
       user.fb_pic_square = auth["info"]["image"]
