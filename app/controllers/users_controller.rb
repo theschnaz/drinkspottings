6ed01d831cc112796fecfe61e4e121bd5ee0a3ce
Gregory Schnese <greg@coovents.com>
2012-06-30 19:59:52 -0400

    small tweak

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posted_and_liked_drinks = Song.find_by_sql ['SELECT "posts".* FROM "posts" WHERE ("posts".user_id = ?) ORDER BY posts.created_at DESC', @user.id]
+    @posted_and_liked_drinks = Post.find_by_sql ['SELECT "posts".* FROM "posts" WHERE ("posts".user_id = ?) ORDER BY posts.created_at DESC', @user.id]
     #posted_and_liked_drinks = Song.find_by_sql ['SELECT "songs".* FROM "songs" WHERE ("songs".user_id = ?) OR (songs.id IN (SELECT song_id from song_likes where liked_by = ?)) ORDER BY songs.created_at DESC', @user.id, @user.id]
   end
 
