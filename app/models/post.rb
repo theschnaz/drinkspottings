8d8fccf4cd8e95aa5d13b595c0b51dac15d4e7a7
Gregory Schnese <greg@coovents.com>
2012-06-30 19:57:01 -0400

    trying to show drinks posted by a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :user
+
   has_attached_file :photo, 
    # :styles => { :medium => "300x300>", :thumb => "100x100>" },
     :storage => :s3,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :posts
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.name = auth["info"]["name"]
