e85505a148184df4371d46530509cad192bb54e8
Gregory Schnese <greg@coovents.com>
2012-07-05 22:52:31 -0400

    working on main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @posted_and_liked_drinks = Post.find_by_sql ['SELECT "posts".* FROM "posts" WHERE ("posts".posted_by = ?) ORDER BY posts.created_at DESC', current_user.id]
 
   end
   
