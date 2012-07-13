920974bcbeca943098b76a833ef8b9d198f5b3a8
Gregory Schnese <greg@coovents.com>
2012-07-12 22:32:23 -0400

    added more tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,52 @@ class PostsController < ApplicationController
       @tag.name = "Whiskey"
       @tag.save
     end
+    if (params[:scotch] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Scotch"
+      @tag.save
+    end
     if (params[:gin] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Gin"
+      @tag.save
+    end
+    if (params[:vodka] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Vodka"
+      @tag.save
+    end
+    if (params[:rum] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Rum"
+      @tag.save
+    end
+    if (params[:tequila] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Tequila"
+      @tag.save
+    end
+    if (params[:beer] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Beer"
+      @tag.save
+    end
+    if (params[:sake] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Sake"
+      @tag.save
+    end
+    if (params[:wine] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Wine"
       @tag.save
     end
     
