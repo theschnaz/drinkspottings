0baad9826eb974e2aafa48cbda97a5cea0103019
Gregory Schnese <greg@coovents.com>
2012-07-15 21:53:27 -0400

    tweaking venues list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     #get nearby places from 4sq
     foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
     @venues =foursquare.venues.nearby(:ll => "40.719193,-73.945241")
-    @venues[(0..10)]
+    @venues = @venues[(0..10)]
   end
 
   def create  
