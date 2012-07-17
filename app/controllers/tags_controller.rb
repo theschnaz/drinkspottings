73cb97f4ceb7746ca1630b5bec458750860e8805
Gregory Schnese <greg@coovents.com>
2012-07-16 23:01:06 -0400

    trying to show drinks on tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,10 +2,12 @@ class TagsController < ApplicationController
 
 
   def show
-    #@tag = Tag.find_by
-    @drink_ids = Tag.find(:all, :conditions => ["name = ?", params[:id]])
+    @tag = Tag.find(:first, :conditions => ["name = ?", params[:id]])
+    #@drinks = Tag.find(:all, :conditions => ["name = ?", params[:id]])
     
-    render :json => @drink_ids
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users", "tags" WHERE ("posts".posted_by = ? AND "users".id = ? AND "tags".drink_id = "posts".id AND "tags".name = ? ) ORDER BY posts.created_at DESC', @user.id, @user.id, params[:id]]
+    
+    #render :json => @drink_ids
     
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     #@posted_and_liked_drinks = Post.find_by_sql ['SELECT "posts".* FROM "posts" WHERE ("posts".posted_by = ? ) ORDER BY posts.created_at DESC', @user.id]
-    @posted_and_liked_drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users" WHERE ("posts".posted_by = ? AND "users".id = ? ) ORDER BY posts.created_at DESC', @user.id, @user.id]
+    #posted and liked drinks
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users" WHERE ("posts".posted_by = ? AND "users".id = ? ) ORDER BY posts.created_at DESC', @user.id, @user.id]
   end
 
 
