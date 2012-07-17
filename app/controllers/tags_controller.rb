9f143cac3f141caab3314b641555ef5452a7235b
Gregory Schnese <greg@coovents.com>
2012-07-16 23:28:54 -0400

    show drinks on tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
     @tag = Tag.find(:first, :conditions => ["name = ?", params[:id]])
     #@drinks = Tag.find(:all, :conditions => ["name = ?", params[:id]])
     
-    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users", "tags" WHERE ("posts".posted_by = "users".id AND "users".id = "tags".user_id AND "tags".drink_id = "posts".id AND "tags".name = ? ) ORDER BY posts.created_at DESC', params[:id]]
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users", "tags" WHERE ("tags".drink_id = "posts".id  AND "tags".user_id = "users".id AND "tags".name = ? ) ORDER BY posts.created_at DESC', params[:id]]
     
     #render :json => @drink_ids
     
