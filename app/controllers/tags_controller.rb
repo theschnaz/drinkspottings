a1cc721d6ad07969aec9f8e86aecef199e214163
Gregory Schnese <greg@coovents.com>
2012-07-16 22:51:29 -0400

    getting drink ids

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
 
   def show
     #@tag = Tag.find_by
-    @drink_ids = Tag.find(:all, :conditions => ["drink_id = ?", params[:id]])
+    @drink_ids = Tag.find(:all, :conditions => ["name = ?", params[:id]])
     
     render :json => @drink_ids
     
