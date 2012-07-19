86ec137f31671db9e5a282e78365638d5d995cdd
Gregory Schnese <greg@coovents.com>
2012-07-18 22:07:03 -0400

    getting venue data for posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     if current_user
       #@user = current_user
       #this will display all drinks from all users
-      @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid FROM "posts", "users" WHERE "posts".posted_by = "users".id ORDER BY posts.created_at DESC']
+      @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id ORDER BY posts.created_at DESC']
     end
 
   end
