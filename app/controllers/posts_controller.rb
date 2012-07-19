d7c1fc5d81f248ba7de865a4fbd091952e78c8f6
Gregory Schnese <greg@coovents.com>
2012-07-18 21:28:32 -0400

    making drink tags work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,84 +21,95 @@ class PostsController < ApplicationController
     @post.posted_by = params[:posted_by]
     @post.save
     
-    unless(Venue.find_by_foursquare_id(params[:venue]))
-      new_venue = Venue.new
-      foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
-      venue = foursquare.venues.find(params[:venue])
+    if params[:venue]
+      unless(Venue.find_by_foursquare_id(params[:venue]) && params[:venue])
+        new_venue = Venue.new
+        foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
+        venue = foursquare.venues.find(params[:venue])
       
-      new_venue.foursquare_id = venue.id
-      new_venue.name = venue.name
-      new_venue.phone = venue.contact['formattedPhone']
-      new_venue.twitter = venue.contact['twitter']
-      new_venue.address = venue.location['address']
-      new_venue.lat = venue.location['lat']
-      new_venue.lng = venue.location['lng']
-      new_venue.postalcode = venue.location['postalCode']
-      new_venue.city = venue.location['city']
-      new_venue.state = venue.location['state']
-      new_venue.country = venue.location['country']
-      new_venue.icon = venue.icon
-      #new_venue.url = venue.url
+        new_venue.foursquare_id = venue.id
+        new_venue.name = venue.name
+        new_venue.phone = venue.contact['formattedPhone']
+        new_venue.twitter = venue.contact['twitter']
+        new_venue.address = venue.location['address']
+        new_venue.lat = venue.location['lat']
+        new_venue.lng = venue.location['lng']
+        new_venue.postalcode = venue.location['postalCode']
+        new_venue.city = venue.location['city']
+        new_venue.state = venue.location['state']
+        new_venue.country = venue.location['country']
+        new_venue.icon = venue.icon
+        #new_venue.url = venue.url
       
-      new_venue.save
+        new_venue.save
+      end
     end
     
     if (params[:whiskey] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Whiskey"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:scotch] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Scotch"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:gin] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Gin"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:vodka] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Vodka"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:rum] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Rum"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:tequila] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Tequila"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:beer] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Beer"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:sake] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Sake"
+      @tag.user_id = current_user.id
       @tag.save
     end
     if (params[:wine] == 'selected')
       @tag = Tag.new
       @tag.drink_id = @post.id
       @tag.name = "Wine"
+      @tag.user_id = current_user.id
       @tag.save
     end
     
-    render :text => @post, :status => 200
+    redirect_to "/"
   
   end
 
