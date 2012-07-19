fabf4f725119a613c851d5c25fc4880a78ce2679
Gregory Schnese <greg@coovents.com>
2012-07-18 22:14:03 -0400

    adding venues pages

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,15 @@
+class VenuesController < ApplicationController
+
+
+  def show
+    @venue = Venue.find(:first, :conditions => ["id = ?", params[:id]])
+    #@drinks = Tag.find(:all, :conditions => ["name = ?", params[:id]])
+    
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "tags", "venues" WHERE ("posts".venue_id = "venues".id AND "tags".drink_id = "posts".id  AND "tags".user_id = "users".id AND "venues".id = ? ) ORDER BY posts.created_at DESC', params[:id]]
+    
+    ##render :json => @drink_ids
+    
+  end
+
+
+end
