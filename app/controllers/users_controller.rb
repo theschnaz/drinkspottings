b8813297e1f4d1497df6598f00a83ca7b5665480
Gregory Schnese <greg@coovents.com>
2012-07-18 21:50:47 -0400

    getting venue data for posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     #@posted_and_liked_drinks = Post.find_by_sql ['SELECT "posts".* FROM "posts" WHERE ("posts".posted_by = ? ) ORDER BY posts.created_at DESC', @user.id]
     #posted and liked drinks
-    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venus_name FROM "posts", "users", "venues" WHERE ("venues".id = "posts".venue_id AND "posts".posted_by = ? AND "users".id = ? ) ORDER BY posts.created_at DESC', @user.id, @user.id]
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id FROM "posts", "users", "venues" WHERE ("posts".posted_by = "users".id AND "posts".venue_id = "venues".id AND "users".id = ? ) ORDER BY posts.created_at DESC', @user.id]
   end
 
 
