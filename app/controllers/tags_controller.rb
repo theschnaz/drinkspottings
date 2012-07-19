99ef2f28db01422778a5554d71688037e98c0d76
Gregory Schnese <greg@coovents.com>
2012-07-18 22:04:29 -0400

    getting venue data for posts

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
     @tag = Tag.find(:first, :conditions => ["name = ?", params[:id]])
     #@drinks = Tag.find(:all, :conditions => ["name = ?", params[:id]])
     
-    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name FROM "posts", "users", "tags", "venues" WHERE ("posts".venue_id = "venues".id AND "tags".drink_id = "posts".id  AND "tags".user_id = "users".id AND "tags".name = ? ) ORDER BY posts.created_at DESC', params[:id]]
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "tags", "venues" WHERE ("posts".venue_id = "venues".id AND "tags".drink_id = "posts".id  AND "tags".user_id = "users".id AND "tags".name = ? ) ORDER BY posts.created_at DESC', params[:id]]
     
     ##render :json => @drink_ids
     
