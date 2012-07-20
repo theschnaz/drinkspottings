40157f1eaf54de8417c8232d61e7fbeaa515ea62
Gregory Schnese <greg@coovents.com>
2012-07-19 22:16:11 -0400

    geo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     
     #get nearby places from 4sq
     foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
-    @venues =foursquare.venues.nearby(:ll => fourvenu)
+    @venues =foursquare.venues.nearby(:ll => fourvenue)
     @venues = @venues[(0..9)]
   end
 
