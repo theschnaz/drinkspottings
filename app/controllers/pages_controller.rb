dc144bb06a0a30706c93ab3a18857dd51fcd2d8b
Gregory Schnese <greg@coovents.com>
2012-07-30 17:05:33 -0400

    if mode is app, show differnt pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
   def home
+  	@mode = params[:mode]
+  
     @title = "Home"
     
     if current_user
       #@user = current_user
       #this will display all drinks from all users
       @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id ORDER BY posts.created_at DESC']
+    end
+    
+    if @mode == 'app'
+      render "home_app"
     end
 
   end
