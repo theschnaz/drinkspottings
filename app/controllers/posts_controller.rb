f1394827d00d4c57652abf9d798bbc902aa6ed8b
Gregory Schnese <greg@coovents.com>
2012-07-29 21:55:43 -0400

    trying to post from app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,119 @@ class PostsController < ApplicationController
     @venues = @venues[(0..9)]
   
   end
+  
+  
+  def create_app
+    @post = Post.find_by_id(params[:drink_id])
+    
+    #assign values
+    @post.name = params[:name]
+    @post.description = params[:description]
+    #by default, set venue id to 0 (no venue)
+    @post.venue_id = 0
+    
+    if params[:venue]
+      #if the venue ID is already in the DB, just post the drink there
+      @post.venue_id = params[:venue]
+      unless(Venue.find_by_foursquare_id(params[:venue]) && params[:venue])
+        new_venue = Venue.new
+        foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
+        venue = foursquare.venues.find(params[:venue])
+      
+        new_venue.foursquare_id = venue.id
+        new_venue.name = venue.name
+        new_venue.phone = venue.contact['formattedPhone']
+        new_venue.twitter = venue.contact['twitter']
+        new_venue.address = venue.location['address']
+        new_venue.lat = venue.location['lat']
+        new_venue.lng = venue.location['lng']
+        new_venue.postalcode = venue.location['postalCode']
+        new_venue.city = venue.location['city']
+        new_venue.state = venue.location['state']
+        new_venue.country = venue.location['country']
+        new_venue.icon = venue.icon
+        #new_venue.url = venue.url
+      
+        new_venue.save
+        
+        #if the venue was pulled from 4sq, then use that ID and post the drink there
+        @post.venue_id = new_venue.id
+      end
+    end
+    
+    
+    @post.posted_by = params[:posted_by]
+    @post.save
+    
+    if (params[:whiskey] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Whiskey"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:scotch] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Scotch"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:gin] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Gin"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:vodka] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Vodka"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:rum] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Rum"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:tequila] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Tequila"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:beer] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Beer"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:sake] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Sake"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    if (params[:wine] == 'selected')
+      @tag = Tag.new
+      @tag.drink_id = @post.id
+      @tag.name = "Wine"
+      @tag.user_id = current_user.id
+      @tag.save
+    end
+    
+    redirect_to "/"
+    
+  
+  end
+  
 
   def new
   	unless current_user
