547c0c2b282fbb04f81d7f677f937a6718965e06
Gregory Schnese <greg@coovents.com>
2012-08-02 23:15:20 -0400

    pulling data from fb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,10 +16,10 @@ class SessionsController < ApplicationController
      user.name = user_data.name
      user.username = user_data.username
      user.email = user_data.email
-     user.fb_pic_square = 'http://graph.facebook.com/' + user_data.id + '/picture?type=square'
-     user.fb_pic_large = 'http://graph.facebook.com/' + user_data.id + '/picture?type=large'
+     user.fb_pic_square = 'http://graph.facebook.com/' + user_data.identifier + '/picture?type=square'
+     user.fb_pic_large = 'http://graph.facebook.com/' + user_data.identifier + '/picture?type=large'
      user.provider = 'facebook'
-     user.uid = user_data.id
+     user.uid = user_data.identifier
      user.save
      
     end
