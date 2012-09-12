db37e3f4312f40ac78d6949e4c9899304c77ef50
Gregory Schnese <greg@coovents.com>
2012-09-11 22:17:25 -0400

    json output should include url to drink now

--- app/controllers/iphones_controller.rb
+++ app/controllers/iphones_controller.rb
@@ -4,7 +4,7 @@ class IphonesController < ApplicationController
 
     @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id ORDER BY posts.created_at DESC']
     
-    render :json => @drinks
+    render :json => @drinks.to_json(:methods => [:post_url])
 
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,8 @@ class Post < ActiveRecord::Base
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+    
+   def post_url
+     post.url(:medium)
+   end
 end
