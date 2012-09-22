a120e0f726f590df1e7c33f37f2897c84b166a7c
Gregory Schnese <greg@coovents.com>
2012-09-22 02:33:58 -0400

    tweaking sql 3

--- app/controllers/iphones_controller.rb
+++ app/controllers/iphones_controller.rb
@@ -20,7 +20,7 @@ class IphonesController < ApplicationController
     north = lat + distance
     south = lat - distance
     
-    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id AND "venues".lat >= \'' + south.to_s + '\' AND "venues".lat <= \'' + north.to_s + '\' AND "venues".lng >= \'' + west.to_s + '\' AND "venues".lng <= \'' + east.to_s + '\' ORDER BY posts.created_at DESC']
+    @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id AND "venues".lat >= \'' + south.to_s + '\' AND "venues".lat <= \'' + north.to_s + '\' AND "venues".lng >= \'' + east.to_s + '\' AND "venues".lng <= \'' + west.to_s + '\' ORDER BY posts.created_at DESC']
     
     render :json => @drinks.to_json(:methods => [:post_url])
   
