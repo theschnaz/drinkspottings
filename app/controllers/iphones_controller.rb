dfdfbea2c6d0162ef2bf04cb0ec295d7e0931789
Gregory Schnese <greg@coovents.com>
2012-09-22 05:11:08 -0400

    geo 6

--- app/controllers/iphones_controller.rb
+++ app/controllers/iphones_controller.rb
@@ -14,7 +14,7 @@ class IphonesController < ApplicationController
     lng = params[:lng].to_f
 
     #distance = 0.0036
-  	distance = 0.006 
+  	distance = 0.007 
   
     west = lng - distance
     east = lng + distance
