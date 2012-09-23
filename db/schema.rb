063705eb695111b718fc0a337fa84fc61d529c93
Gregory Schnese <greg@coovents.com>
2012-09-22 20:44:09 -0400

    creating to do lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120719013810) do
+ActiveRecord::Schema.define(:version => 20120923003817) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(:version => 20120719013810) do
     t.datetime "updated_at"
     t.string   "image"
     t.integer  "venue_id"
+  end
+
+  create_table "saves", :force => true do |t|
+    t.integer  "drink_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", :force => true do |t|
