871675de2bb4930dd4899d18de85ecc5a765125e
Gregory Schnese <greg@coovents.com>
2012-09-22 22:29:33 -0400

    fixing more bugs

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,8 +7,9 @@ class VenuesController < ApplicationController
     
     @drinks = Post.find_by_sql ['SELECT DISTINCT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "tags", "venues" WHERE ("posts".venue_id = "venues".id AND "tags".drink_id = "posts".id  AND "tags".user_id = "users".id AND "venues".id = ? ) ORDER BY posts.created_at DESC', params[:id]]
     
-    @saved_drinks = Save.find_by_sql ['SELECT "saves".drink_id as ID FROM "saves" WHERE "saves".user_id = \'' + current_user.id.to_s + '\'']
-    
+    if current_user
+      @saved_drinks = Save.find_by_sql ['SELECT "saves".drink_id as ID FROM "saves" WHERE "saves".user_id = \'' + current_user.id.to_s + '\'']
+    end
     ##render :json => @drink_ids
     
   end
