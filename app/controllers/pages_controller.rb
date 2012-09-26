2386db261335ff987d2eaca7bfe67d71fdb71bf6
Gregory Schnese <greg@coovents.com>
2012-09-25 22:25:06 -0400

    adding saved drinks view 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,7 +28,7 @@ class PagesController < ApplicationController
     if current_user
       #@user = current_user
       #this will display all drinks from all users
-      @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues", "saves" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id AND "posts".id = "saves".drink_id AND "saves".user_id = 														\'' + current_user.id.to_s + '\' ORDER BY posts.created_at DESC']
+      @drinks = Post.find_by_sql ['SELECT "posts".*, "users".name as personname, "users".fb_pic_square, "users".id as userid, "venues".id as venue_id, "venues".name as venue_name FROM "posts", "users", "venues", "saves" WHERE "posts".venue_id = "venues".id AND "posts".posted_by = "users".id AND "posts".id = "saves".drink_id AND "saves".user_id = 														\'' + current_user.id.to_s + '\' ORDER BY saves.id DESC']
     end
     
     if current_user
