61fa0d3f684731401f784e408ecdd517ca0d2124
Gregory Schnese <greg@coovents.com>
2012-10-30 23:16:50 -0400

    long way, using uid 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
    end
    
    def saved_by
-     Save.find_by_sql ['SELECT "users".uid FROM "users", "saves" WHERE "saves".user_id = "users".id AND "saves".id = \'' + self.id.to_s + '\'']
+     Save.find_by_sql ['SELECT "users".uid FROM "users", "saves" WHERE "saves".user_id = "users".id AND "saves".drink_id = \'' + self.id.to_s + '\'']
    end
    
    def tags
