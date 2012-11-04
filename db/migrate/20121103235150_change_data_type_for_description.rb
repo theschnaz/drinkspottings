735a4f28b775eb21dfb037061152a2dbab83bb1b
Gregory Schnese <greg@coovents.com>
2012-11-03 20:27:04 -0400

    same thing...

--- db/migrate/20121103235150_change_data_type_for_description.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForDescription < ActiveRecord::Migration
-  def up
-  	change_column :posts, :description, :text, :limit => nil
-  end
-
-  def down
-  	change_column :posts, :description, :text, :limit => '255'
-  end
-end
