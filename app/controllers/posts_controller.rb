dab35afb0ae0a2daf5f1b49e1b398813ce8376b5
Gregory Schnese <greg@coovents.com>
2012-11-13 23:46:09 -0500

    adding form validation now 9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
   
   def new_app
-  	@error = params[:error]
+  	@error = params[:flash]
   
   	@mobile = true
   	@post = Post.find_by_id(params[:drink_id])
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   
   def create_app
   	if params[:post][:name]
-  		redirect_to request.referer :error => 'name' and return
+  		redirect_to :back, :flash => 'error' and return
   	end
   	
     @post = Post.find_by_id(params[:drink_id])
