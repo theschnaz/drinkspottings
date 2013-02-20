46feeeebb946330e9e3327948ba366f39a0cb34e
Gregory Schnese <greg@coovents.com>
2013-02-19 20:43:15 -0500

    tweaking 4sq 6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     
     #get nearby places from 4sq
     foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
-    @venues =foursquare.venues.search(:ll => fourvenue, :query => "walter" )
+    @venues =foursquare.venues.search(:ll => fourvenue )
     #@venues = @venues[(0..15)]
   end
   
