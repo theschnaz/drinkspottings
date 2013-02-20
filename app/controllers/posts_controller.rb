54d3f1e89c7a6b22ce12e293805099cbc21e517f
Gregory Schnese <greg@coovents.com>
2013-02-19 23:28:15 -0500

    more test 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
     @venues = foursquare.venues.search(:ll => fourvenue, :query => "walter food")
     #@venues =foursquare.venues.nearby(:ll => fourvenue)
-    render :text => @venues.places
+    render :text => @venues[places]
     #@venues = @venues[(0..15)]
   end
   
