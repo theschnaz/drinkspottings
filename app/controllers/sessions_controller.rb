2154ccac82f4fdb28bb0f0c8521f08d5ee76b9ef
Gregory Schnese <greg@coovents.com>
2013-05-29 21:58:31 -0400

    newsletter column for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
      user_data = FbGraph::User.fetch(uid)
      user = User.new
      user.name = user_data.name
+     user.newsletter = params[:newsletter]
      user.username = user_data.username
      user.email = user_data.email
      user.fb_pic_square = 'http://graph.facebook.com/' + user_data.identifier + '/picture?type=square'
