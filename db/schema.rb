81fa60f542c072c0a3d8c624cf5ec95ce7f88256
Gregory Schnese <greg@coovents.com>
2013-05-29 21:53:11 -0400

    adding newsletter column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121104003304) do
+ActiveRecord::Schema.define(:version => 20130530015114) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20121104003304) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "newsletter"
   end
 
   create_table "venues", :force => true do |t|
