7a585b6dedd9883fd4e871611522998758098d31
Gregory Schnese <greg@coovents.com>
2013-07-24 23:05:15 -0400

    users should be able to post now 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -209,7 +209,7 @@ class PostsController < ApplicationController
    
    	  me = FbGraph::User.me(user.facebook_key)
    	  link = me.link!(
-   	    :link => 'http://drinkspottings.com/image.html',
+   	    :link => 'http://www.drinkspottings.com/image.html',
    	    :message => @post.description
 	  )
 	#end
