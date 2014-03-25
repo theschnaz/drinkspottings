d4e91d4965838e857369627bd70d62897b4f8cb2
Gregory Schnese <theschnaz@gmail.com>
2014-03-24 23:22:15 -0400

    testing line 50 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
     #get nearby places from 4sq
     foursquare = Foursquare::Base.new("G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT", "T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV")
     #@venues = foursquare.venues.search(:ll => fourvenue, :query => "walter food")
-    @venues = foursquare.venues.search(:ll => '40.6922881, 73.9592273', :query => "coffee")
+    @venues = foursquare.venues.search(:ll => '40.6922881, -73.9592273', :query => "coffee")
     @venues = @venues["nearby"]
     #@venues = @venues["places"]
     #@venues =foursquare.venues.nearby(:ll => fourvenue)
