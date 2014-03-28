a39abe179aabf03b9089804a1a79efda17903657
Gregory Schnese <theschnaz@gmail.com>
2014-03-27 23:15:00 -0400

    adding api version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
     fourvenue = params[:lat] + "," + params[:long]
     
     #get nearby places from 4sq
-    foursquare = Foursquare2::Client.new(:client_id => 'G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT', :client_secret => 'T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV')
+    foursquare = Foursquare2::Client.new(:client_id => 'G24WDWF3I0VR0HEJEXYOQ4MTQ5ZW21NVEAQKKVVQDGDAFHBT', :client_secret => 'T0SBP3DWC14VZ1ZI1ADJABS2SPQBQ4G204P1FEDVSUKQNFOV', :api_version => '20120505')
     #@venues = foursquare.venues.search(:ll => fourvenue, :query => "walter food")
     @venues = foursquare.search_venues(:ll => fourvenue)
     @venues = @venues["nearby"]
